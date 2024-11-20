1213a8ed39bd620f9d30b93ba8ebf039d45777d2
Atharva <atharva0276@gmail.com>
2024-11-20 14:24:50 +0530

    Add Software Developer Roadmap Basic Layout

---
+++ src/components/static-roadmaps/software-engineering/CppRoadmap.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+import { ArrowLeft } from 'lucide-react';
+
+const CppRoadmap = () => {
+  return (
+    <div className="min-h-screen pt-20 pb-12">
+    </div>
+  );
+};
+
+export default CppRoadmap; 
